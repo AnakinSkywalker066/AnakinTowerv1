2b0f3d5c5de475978211efafec9d897995ef7e46
DrVarsity <Leegovols@gmail.com>
2023-04-19 21:20:24 -0400

    Update

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+﻿namespace starwarsmod;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0";
+    public const string Name = "Star Wars Mod";
+
+    public const string Description = "Work In Progress Mod!";
+
+    public const string RepoOwner = "AnakinSkywalker066";
+    public const string RepoName = "test-btd6-monorepo";
+
+    public const string SubPath = "StarWarsMod";
+    public const string DllName = "StarWarsMod.dll";
+}
--- StarWars.cs
+++ StarWars.cs
@@ -2,19 +2,6 @@
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace starwarsmod;
 
-public static class ModHelperData
-{
-    public const string WorksOnVersion = "36.0";
-    public const string Version = "1.0";
-    public const string Name = "Star Wars Mod";
-    public const string Description = "#Adds Star Wars to BTD6!" + "#Currently working on putting in 3D Models of Anakin!";
-    public const string RepoOwner = "AnakinSkywalker066";
-    public const string RepoName = "StarWarsMod";
-    public const string Icon = "Star.png";
-    public const string DllName = "StarWarsMod.dll";
-    public const string Author = "DrVarsity";
-}
-
 public class StarWars : BloonsTD6Mod
 {
     public override void OnApplicationStart() 
