03c805777701b73526598b98a60fbe8a732cc2eb
DrVarsity <Leegovols@gmail.com>
2023-04-19 20:58:16 -0400

    Update

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@
         public const string Version = "1.0";
         public const string Name = "Star Wars Mod";
         public const string Description = "#Adds Star Wars to BTD6!" + "#Currently working on putting in 3D Models of Anakin!";
-        public const string RepoOwner = "anakinskywalker066";
+        public const string RepoOwner = "AnakinSkywalker066";
         public const string RepoName = "StarWarsMod";
         public const string Icon = "Star.png";
     }
