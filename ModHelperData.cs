354d073b1d931ec4f72340c328e444e63abc8f35
DrVarsity <Leegovols@gmail.com>
2023-04-30 11:51:46 -0400

    Update to Mod Helper Data

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "1.0";
     public const string Name = "Star Wars Mod";
 
-    public const string Description = "Work In Progress Mod!";
+    public const string Description = "Anakin Skywalker is Now In BTD6 3D Model Included!!!" + "Only Top Path Available Right Now..." + "Thanks to everyone on the 1330 Studios Discord server who helped me make this mod." + "This is the first Mod I Made!!!";
 
     public const string RepoOwner = "AnakinSkywalker066";
     public const string RepoName = "StarWarsMod";
